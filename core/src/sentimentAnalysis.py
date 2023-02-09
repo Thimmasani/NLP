255649f74f16a896dba2e934177fa3407262ffd8
Dinesh Reddy Thimmasani <thimmasanidineshreddy19@gmail.com>
2023-02-09 14:12:49 +0530

    Create sentimentAnalysis.py

---
+++ core/src/sentimentAnalysis.py
@@ -0,0 +1,3 @@
+from core.transformers.sentencePreprocessing import preprocess
+
+#print(preprocess.process_tweet("@taufik_toda we made our final design please check that out :) https://t.co/EXt098Yq1b"))
